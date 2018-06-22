f05a0f926eb211bbbbc666d6f1d60e79af47c959
dej27 <22531726+dej27@users.noreply.github.com>
2018-06-22 13:12:17 -0500

    Add files via upload

---
+++ DJ_Get-ADPwdLastSet.ps1
@@ -0,0 +1,45 @@
+﻿Function DJ_Get-ADPwdLastSet
+{
+	<#
+		.SYNOPSIS
+		Gets last AD password change for specified user	
+	
+		.DESCRIPTION
+		Gets last AD password change for specified user
+	
+		.EXAMPLE
+		DJ_Get-ADPwdLastSet -SamAccountName jsmith
+		jsmith, bjones | DJ_Get-ADPwdLastSet
+		
+		.NOTES
+		Author: Delonte E. Johnson
+	
+	#>
+	
+	[cmdletbinding()]
+	
+	param
+	(
+		[parameter(Mandatory=$true, ValueFromPipeline=$true)][string[]]$SamAccountName
+	)
+	
+	begin { }
+	
+	process
+	{
+		$user = Get-ADUser "$SamAccountName" -Properties *
+		
+		If ($user.pwdlastset -ne $null)
+		{
+			$DateTime = [wmi]::new().ConvertFromDateTime($user.pwdlastset)
+			[wmi]::new().ConvertToDateTime($DateTime)
+		}
+		Else
+		{
+			Write-Error 'Unable to retrieve password last set date. Check to make sure your account has the appropriate permissions to view this information'	
+		}
+		
+	}
+	
+	end { }
+}
