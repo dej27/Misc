9c8af0ce9391ed8cb16d87d4b57d38a1b794fd99
dej27 <22531726+dej27@users.noreply.github.com>
2018-06-22 13:13:28 -0500

    Add files via upload

---
+++ DJ_Out-BASE64.ps1
@@ -0,0 +1,34 @@
+﻿Function DJ_Out-BASE64
+{
+	<#
+		.SYNOPSIS
+		Outputs base64 encoded string	
+	
+		.DESCRIPTION
+		Takes in unicode string then transforms into base64 string
+	
+		.EXAMPLE
+		DJ_Out-BASE64 -InputString 'test'
+		'test' | DJ_Out-BASE64
+	
+		.NOTES
+		Author: Delonte E Johnson
+	#>
+	
+	[cmdletbinding()]
+	
+	Param
+	(
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		[string]$InputString
+	)
+	
+	Begin { }
+	
+	Process
+	{
+		[Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($InputString))
+	}
+	
+	End { }
+}
